b5076cce78f78047ad839b659d7322f7f9c8516a
qhu <qiang.hu@roswellpark.org>
2022-12-30 11:04:35 -0500

    more options

--- openai_story.py
+++ openai_story.py
@@ -6,10 +6,10 @@ from PIL import Image
 # Set up the OpenAI API client
 openai.api_key = st.secrets["key"]
 
-def gen_story(prompt):
+def gen_story(prompt, kind):
     # Set up the model and prompt
     model_engine = "text-davinci-003"
-    prompt = "make a story for kids. {}".format(prompt)
+    prompt = "make a {} story for kids. {}".format(kind, prompt)
     
     # Generate a response
     completion = openai.Completion.create(
@@ -25,9 +25,9 @@ def gen_story(prompt):
     #print(response)
     return response
 
-def gen_img(prompt, path="/tmp/story.png"):
+def gen_img(prompt, kind, path="/tmp/story.png"):
     ## img
-    prompt = "children comic, picture book, illustration. cute eyes, detail face, not scary. {}".format(prompt)
+    prompt = "children comic, picture book, illustration. {}. detail face, not scary, cute eyes. {}".format(kind, prompt)
     response = openai.Image.create(
         prompt=prompt,
         n=1,
@@ -40,12 +40,15 @@ def gen_img(prompt, path="/tmp/story.png"):
 
 
 prompt = st.sidebar.text_area("Start your story:")
+kind = st.sidebar.selectbox("Select story type:", options = ["funny", "fairy tale", "sci-fi", "national geographic"])
+plot = st.sidebar.radio("Whether to draw image:", options = ["Yes", "No"])
 start = st.sidebar.button("Submit")
 
 if start:
-    story = gen_story(prompt) 
+    story = gen_story(prompt, kind) 
     st.write(story)
 
-    img_url = gen_img(prompt)
-    image = Image.open(img_url)
-    st.image(image)
+    if plot == "Yes":
+        img_url = gen_img(prompt, kind)
+        image = Image.open(img_url)
+        st.image(image)
